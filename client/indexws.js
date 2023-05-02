8558e3a845170567935cd1e9d450d352f40aa952
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-05-02 14:08:32 +0530

    u commit

--- client/indexws.js
+++ client/indexws.js
@@ -34,6 +34,7 @@ let constraints = {
 const init=async()=>{
     localStream = await navigator.mediaDevices.getUserMedia(constraints)
 document.getElementById('user-1').srcObject = localStream
+document.getElementById('user-1').muted = true
 
 }
 init()
