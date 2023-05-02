1ea4a90f0ca0b61bba0588f74356334d1a04257c
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-05-02 12:53:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const color = require("colors");
+var http = require("http");
+const socket = require("socket.io");
+const dotenv = require("dotenv");
+const {
+  disconnectUser,
+  addUser,
+  getroomuser,
+  getCurrentUser,
+} = require("./user");
+dotenv.config("/.env");
+
+app.use(express.static(path.join(__dirname, "client")));
+
+//routes
+app.get("/", (req, res, next) => {
+  return res.send("Okay");
+});
+
+const server = http.createServer(app);
+
+const port = process.env.PORT;
+
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`.bgGreen)
+);
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("UserJoined", ({  roomId }) => {
+    if (!getCurrentUser(socket.id)) {
+      if (getroomuser(roomId).length< 2) {
+        const user = addUser(socket.id, roomId);
+        socket.join(user.room)
+        socket.broadcast.to(user.room).emit("PeerJoined", {id:user.id,room:user.room});
+        
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = disconnectUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('PeerLeft',(user.id))
+
+    }
+  });
+
+  socket.on("MessagePeer", (data,MemberId) => {
+    const user = getCurrentUser(socket.id);
+    if (user) {
+
+        socket.to(user.room).emit('MessageFromPeer',data,MemberId)
+    }
+  });
+});
---
+++ user.js
@@ -0,0 +1,33 @@
+let users=[]
+
+
+const addUser=(id,room)=>{
+const user={id,room}
+users.push(user)
+console.log("d",users)
+return user;
+}
+
+
+const getCurrentUser=(id)=>{
+return users.find(user=>user.id==id)
+}
+
+const disconnectUser=(id)=>{
+    const index= users.findIndex((user)=>user.id==id)
+    if(index!=-1){
+    return users.splice(index,1)[0]
+    }
+    }
+
+const getroomuser=(room)=>{
+
+    const roomusers=users.filter((user)=>{
+        if(room==user.room){
+            return true
+        }
+    })
+    console.log(roomusers)
+    return roomusers
+}
+module.exports={addUser,getCurrentUser,disconnectUser,getroomuser}
